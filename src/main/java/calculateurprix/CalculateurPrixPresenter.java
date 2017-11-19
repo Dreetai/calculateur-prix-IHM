e87ae6b8dbac5d25bf6a8cee44247f7babbcc821
Dreetai <jongrab@gmail.com>
2017-11-19 14:54:26 +0100

    Etape 3

--- src/main/java/calculateurprix/CalculateurPrixPresenter.java
+++ src/main/java/calculateurprix/CalculateurPrixPresenter.java
@@ -1,15 +1,29 @@
 package calculateurprix;
 
+
 public class CalculateurPrixPresenter {
+
+    public enum Pays {
+        Belgique("Belgique", 21), Danemark("Danemark",25), Allemagne("Allemagne",19), Grèce("Grèce",23), Espagne("Espagne",21), France("France",20), Italie("Italie",22), Chypre("Chypre",19), Luxembourg("Luxembourg",15), Pays_Bas("Pays Bas",21), Autriche("Autriche",20), Portugal("Portugal",23), Suède("Suède",25);
+        private final String key;
+        private final Integer value;
+        Pays(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+        public Integer getValue() { return value; }
+        public String toString() { return key; }
+    }
     private final CalculateurPrixView calculateurPrixView;
 
     public CalculateurPrixPresenter(CalculateurPrixView calculateurPrixView) {
         this.calculateurPrixView = calculateurPrixView;
     }
 
-    public void onComputeButtonClicked(String montantArticleAsText, String quantiteArticleAsText) {
-        calculateurPrixView.afficherMontant(Float.valueOf(montantArticleAsText)*Float.valueOf(quantiteArticleAsText));
+    public void onComputeButtonClicked(String montantArticleAsText, String quantiteArticleAsText, Pays pays) {
+        float montant = Float.valueOf(montantArticleAsText)*Float.valueOf(quantiteArticleAsText);
+        calculateurPrixView.afficherMontantHT(montant);
+        calculateurPrixView.afficherMontantTTC(montant*(pays.getValue()/100f+1));
     }
-
 
 }
--- src/main/java/calculateurprix/CalculateurPrixView.java
+++ src/main/java/calculateurprix/CalculateurPrixView.java
@@ -1,8 +1,6 @@
 package calculateurprix;
 
-import static java.awt.BorderLayout.EAST;
-import static java.awt.BorderLayout.SOUTH;
-import static java.awt.BorderLayout.WEST;
+
 import static javax.swing.JOptionPane.ERROR_MESSAGE;
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -15,6 +13,7 @@ public class CalculateurPrixView extends JFrame {
 
     private final CalculateurPrixPresenter presenter;
     private JFormattedTextField montantHTTextField;
+    private JFormattedTextField montantTTCTextField;
 
     public CalculateurPrixView() throws HeadlessException {
         super("Calculateur de prix");
@@ -26,45 +25,68 @@ public class CalculateurPrixView extends JFrame {
         prixArticleTextField.setToolTipText("Entrez ici le montant d'un article");
 
         JLabel quantiteLabel = new JLabel("Quantité : ");
+        //quantiteLabel.setMinimumSize(new Dimension(100, 100));
+        //quantiteLabel.setPreferredSize(new Dimension(10,20));
+        //quantiteLabel.setMaximumSize(new Dimension(100, 100));
         JTextField quantiteTextField = new JTextField(10);
         quantiteLabel.setLabelFor(quantiteTextField);
         quantiteTextField.setToolTipText("Entrez ici la quantité d'article");
+
+        JLabel paysLabel = new JLabel("Pays : ");
+        JComboBox<CalculateurPrixPresenter.Pays> paysJComboBox = new JComboBox<>(CalculateurPrixPresenter.Pays.values());
+        paysLabel.setLabelFor(paysJComboBox);
 
         JLabel montantHTLabel = new JLabel("Montant HT : ");
         montantHTTextField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
         montantHTTextField.setEditable(false);
         montantHTLabel.setLabelFor(montantHTTextField);
 
-        JButton computeButton = new JButton("Calculer");
-        computeButton.addActionListener(e -> this.presenter.onComputeButtonClicked(prixArticleTextField.getText(), quantiteTextField.getText()));
+        JLabel montantTTCLabel = new JLabel("Montant TTC (France) : ");
+        montantTTCTextField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
+        montantTTCTextField.setEditable(false);
+        montantTTCLabel.setLabelFor(montantTTCTextField);
 
-        JPanel contentPane = new JPanel();
+        JButton computeButton = new JButton("Calculer");
+        computeButton.addActionListener(e -> this.presenter.onComputeButtonClicked(prixArticleTextField.getText(), quantiteTextField.getText(),(CalculateurPrixPresenter.Pays) paysJComboBox.getSelectedItem()));
+
+
+        JPanel contentPane = new JPanel(new GridLayout(0,3));
         setContentPane(contentPane);
 
         JPanel labelPane = new JPanel(new GridLayout(0, 1));
         labelPane.add(prixArticleLabel);
         labelPane.add(quantiteLabel);
+        labelPane.add(paysLabel);
         labelPane.add(montantHTLabel);
+        labelPane.add(montantTTCLabel);
 
         JPanel fieldPane = new JPanel(new GridLayout(0, 1));
         fieldPane.add(prixArticleTextField);
         fieldPane.add(quantiteTextField);
+        fieldPane.add(paysJComboBox);
         fieldPane.add(montantHTTextField);
+        fieldPane.add(montantTTCTextField);
+
+        JPanel buttonPane = new JPanel(new GridBagLayout());
+        buttonPane.add(computeButton);
 
         contentPane.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         add(labelPane);
         add(fieldPane);
-        add(computeButton);
+        add(buttonPane);
+
 
         prixArticleTextField.requestFocus();
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void afficherMontant(float montant){
+    public void afficherMontantHT(float montant){
         this.montantHTTextField.setValue(montant);
     }
-
+    public void afficherMontantTTC(float montant){
+        this.montantTTCTextField.setValue(montant);
+    }
 
     public void afficherErreur(String message) {
         showMessageDialog(this, message, "Erreur", ERROR_MESSAGE);
